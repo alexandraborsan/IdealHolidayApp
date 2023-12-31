88cf48ac8c332a703c554a19834c031e39b19715
Alexandra Borsan <alexandra.borsan@gmail.com>
2023-12-31 13:53:18 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace IdealHolidayApp
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void OnCounterClicked(object sender, EventArgs e)
+        {
+            count++;
+
+            if (count == 1)
+                CounterBtn.Text = $"{count} Holiday you could've booked";
+            else
+                CounterBtn.Text = $"{count} Holidays you could've booked";
+
+            SemanticScreenReader.Announce(CounterBtn.Text);
+        }
+    }
+}
